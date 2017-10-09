a0ab395490de86fe88a9e63f795a88c28a8b97c6
Abhijit Kulkarni <akulkarni@meetveracity.com>
2017-10-09 15:36:41 -0400

    ST-1041: Spring Boot Admin Enablement Changes

--- src/main/java/gov/va/ascent/dashboard/AscentDashboardApplication.java
+++ src/main/java/gov/va/ascent/dashboard/AscentDashboardApplication.java
@@ -1,41 +1,126 @@
 package gov.va.ascent.dashboard;
 
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import org.springframework.core.annotation.Order;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CsrfFilter;
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.security.web.csrf.CsrfTokenRepository;
+import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
+import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.util.WebUtils;
+
+import de.codecentric.boot.admin.config.EnableAdminServer;
 
 /**
  * An <tt>Ascent Dashboard Application</tt> enabled for Spring Boot Application, 
  * Spring Cloud Netflix Hystrix circuit breakers, Hystrix Dashboard and Turbine.
  *
  */
-@SpringBootApplication
+@Configuration
+@EnableAutoConfiguration
+@AutoConfigureAfter(SecurityAutoConfiguration.class)
 @EnableDiscoveryClient
+@EnableAdminServer
+@SpringBootApplication
 @EnableHystrix
 @EnableHystrixDashboard
 @Import(TurbineConfiguration.class)
-public class AscentDashboardApplication {
+public class AscentDashboardApplication extends WebSecurityConfigurerAdapter {
+	
+	@Autowired
+    private SecurityProperties securityProperties;
 
 	public static void main(String[] args) {
 		SpringApplication.run(AscentDashboardApplication.class, args);
 	}
 	
-	@Configuration
-	@ConditionalOnProperty(name = "security.basic.enabled", havingValue = "true", matchIfMissing = true)
-	@Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
-	protected static class ApplicationSecurity extends WebSecurityConfigurerAdapter {
-		@Override
-		public void configure(WebSecurity web) throws Exception {
-		    web.ignoring().antMatchers("/turbine.stream");
-		}
+	@Bean
+	@ConfigurationProperties("spring.boot.admin.auth")
+	public BasicAuthFilter basicAuthFilter() {
+		return new BasicAuthFilter();
 	}
+	
+	@Autowired
+	  protected void configure(AuthenticationManagerBuilder auth) {
+	    addInMemoryAuthenticationProvider(auth);
+	  }
+	
+	@Override
+	public void configure(HttpSecurity http) throws Exception {
+		
+		http.httpBasic().and()
+				.authorizeRequests()
+				.antMatchers(HttpMethod.POST, "/api/applications").permitAll()//
+				.antMatchers(HttpMethod.GET, "/health").permitAll()//
+				.antMatchers(HttpMethod.GET, "/info").permitAll()//
+				.antMatchers("/manage/health").permitAll()//
+				.anyRequest().authenticated()//
+				.and().csrf().ignoringAntMatchers("/api/**", "/manage/**")
+				.csrfTokenRepository(csrfTokenRepository()).and()
+				.addFilterAfter(csrfHeaderFilter(), CsrfFilter.class);
+	}
+	
+	private void addInMemoryAuthenticationProvider(AuthenticationManagerBuilder auth) {
+	    try {
+	      auth.inMemoryAuthentication()
+	          .withUser(securityProperties.getUser().getName())
+	          .password(securityProperties.getUser().getPassword())
+	          .roles(securityProperties.getUser().getRole().stream().toArray(String[]::new));
+	    } catch (Exception ex) {
+	      throw new IllegalStateException("Cannot add InMemory users!", ex);
+	    }
+	  }
+	
+	private Filter csrfHeaderFilter() {
+		return new OncePerRequestFilter() {
+			@Override
+			protected void doFilterInternal(HttpServletRequest request,
+					HttpServletResponse response, FilterChain filterChain)
+							throws ServletException, IOException {
+				CsrfToken csrf = (CsrfToken) request.getAttribute(CsrfToken.class.getName());
+				if (csrf != null) {
+					Cookie cookie = WebUtils.getCookie(request, "XSRF-TOKEN");
+					String token = csrf.getToken();
+					if (cookie == null || token != null && !token.equals(cookie.getValue())) {
+						cookie = new Cookie("XSRF-TOKEN", token);
+						cookie.setPath("/");
+						response.addCookie(cookie);
+					}
+				}
+				filterChain.doFilter(request, response);
+			}
+		};
+	}
+
+	private CsrfTokenRepository csrfTokenRepository() {
+		HttpSessionCsrfTokenRepository repository = new HttpSessionCsrfTokenRepository();
+		repository.setHeaderName("X-XSRF-TOKEN");
+		return repository;
+	}
+	
 }
