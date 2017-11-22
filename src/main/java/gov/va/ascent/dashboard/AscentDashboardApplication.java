f41d0c957bbd19810a687829cc5bdecaeb53b3d8
Raju <raju.thota@icfi.com>
2017-11-22 11:26:15 -0500

    ST-1058 - Fixed fortify vioaltion

--- src/main/java/gov/va/ascent/dashboard/AscentDashboardApplication.java
+++ src/main/java/gov/va/ascent/dashboard/AscentDashboardApplication.java
@@ -87,25 +87,29 @@ public class AscentDashboardApplication extends WebSecurityConfigurerAdapter {
 	  }
 	
 	private Filter csrfHeaderFilter() {
-		return new OncePerRequestFilter() {
-			@Override
-			protected void doFilterInternal(HttpServletRequest request,
-					HttpServletResponse response, FilterChain filterChain)
-							throws ServletException, IOException {
-				CsrfToken csrf = (CsrfToken) request.getAttribute(CsrfToken.class.getName());
-				if (csrf != null) {
-					Cookie cookie = WebUtils.getCookie(request, "XSRF-TOKEN");
-					String token = csrf.getToken();
-					if (cookie == null || token != null && !token.equals(cookie.getValue())) {
-						cookie = new Cookie("XSRF-TOKEN", token);
-						cookie.setPath("/");
-						response.addCookie(cookie);
-					}
-				}
-				filterChain.doFilter(request, response);
-			}
-		};
+		return new RestOncePerRequestFilter() ;
 	}
+	
+	private class RestOncePerRequestFilter extends OncePerRequestFilter {
+		@Override
+		protected void doFilterInternal(HttpServletRequest request,
+				HttpServletResponse response, FilterChain filterChain)
+						throws ServletException, IOException {
+			CsrfToken csrf = (CsrfToken) request.getAttribute(CsrfToken.class.getName());
+			if (csrf != null) {
+				Cookie cookie = WebUtils.getCookie(request, "XSRF-TOKEN");
+				String token = csrf.getToken();
+				if (cookie == null || token != null && !token.equals(cookie.getValue())) {
+					cookie = new Cookie("XSRF-TOKEN", token);
+					cookie.setPath("/");
+					response.addCookie(cookie);
+				}
+			}
+			filterChain.doFilter(request, response);
+		}
+	}
+	
+
 
 	private CsrfTokenRepository csrfTokenRepository() {
 		HttpSessionCsrfTokenRepository repository = new HttpSessionCsrfTokenRepository();
